6101191856570a17beeecf608bd0209b01100189
jimi <15570350453@163.com>
2018-11-12 17:45:45 +0800

    日志和权限检查

---
+++ app/src/main/java/com/example/dxcommonlib/DXApplication.java
@@ -0,0 +1,9 @@
+package com.example.dxcommonlib;
+
+/**
+ * author: tonydeng
+ * mail : tonydeng@hxy.com
+ * 2018/11/12
+ */
+public class DXApplication {
+}
---
+++ commonutil/src/main/java/com/daxiong/commonutil/permission/PermissonParams.java
@@ -0,0 +1,13 @@
+package com.daxiong.commonutil.permission;
+
+/**
+ * author: tonydeng
+ * mail : tonydeng@hxy.com
+ * 2018/11/12
+ * 初始化日志库
+ */
+public class PerrmissonParams {
+
+
+    public void init
+}
